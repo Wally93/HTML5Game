760cf774e2b2a2ee28094fb33e6deb5e076023dc
Wally Jniyah <ojniyah@gmail.com>
2017-07-29 23:44:23 -0400

    working out the logic

--- Javascript/App.js
+++ Javascript/App.js
@@ -1,2 +1,47 @@
-var  ctx = document.getElementById('ctx').getContext('2d');
-ctx.fillText('Hello', 50,  50);
+var
+canvas,
+ctx,
+width,
+height,
+frames = 0,
+score = 0,
+best = 0,
+
+currentstate,
+states = {
+    Splash: 0, Game: 1, Score: 2
+},
+bird = {},
+pipes = {};
+
+function main() {
+        canvas = document.createElement("canvas");
+
+        width = window.innerWidth;
+        heigth = window.innerHeigth;
+            if (width >= 500) {
+                  width = 320;
+                  heigth = 480;
+                  canvas.style.border = "1xp solid #000";
+        }
+        canvas.width = width;
+        canvas.heigth = heigth;
+        ctx = canvas.getContext("2d");
+        document.body.appendChild(canvas);
+
+        var img = new  Image();
+        img.onload = function() {
+          console.log("test");
+        };
+        img.src = "res/sheet.png";
+}
+
+        function run() {
+
+        }
+        function update() {
+
+        }
+        function render() {
+
+        }
